9a1947f74cca5b0f1fab7da4149c7c9f85139127
Braden Lawrence <lawrencebradenm@gmail.com>
2017-06-10 21:28:41 +0000

    Add Home, Index, and Listen routes

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 // PACKAGES
 var express = require("express"),
     app     = express()
+    
+// GENERAL APP SETTINGS
+app.set("view engine", "ejs")
+
+// ROUTES
+//  - Home
+app.get("/", function(request, response){
+    response.redirect("/portfolio")
+})
+
+//  - Index
+app.get("/portfolio", function(request, response){
+    response.render("index")
+})
+
+//  - Show
+
+//  - New
+
+//  - Create
+
+//  - Edit
+
+//  - Update
+
+//  - Destroy
+
+//  - Listen
+app.listen(process.env.PORT, process.env.ID, function(){
+    console.log("Portfolio site is running...")
+})
