d730f4eb58d02c8b2b4e0a8e2276064fddace87f
Braden Lawrence <lawrencebradenm@gmail.com>
2017-08-12 21:23:39 +0000

    Update project model to use a single link per project

--- models/project.js
+++ models/project.js
@@ -2,22 +2,22 @@ var mongoose = require("mongoose"),
     Image    = require("./image.js")
 
 var projectSchema = new mongoose.Schema({
-    title:  String,
-    date:   Date,
-    cover:  {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Image"
-    },
-    images: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Image"
-        }
-    ],
+    title:          String,
+    date:           Date,
+    cover:          {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Image"
+                    },
+    images:         [
+                        {
+                            type: mongoose.Schema.Types.ObjectId,
+                            ref: "Image"
+                        }
+                    ],
     description:    String,
-    client: String,
-    media:  [String],
-    links:  String,
+    client:         String,
+    media:          [String],
+    link:           String,
     
 })
 
