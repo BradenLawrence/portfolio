413bdb4b30966ed00aa50ceafa03e64977a8e990
Braden Lawrence <lawrencebradenm@gmail.com>
2017-09-02 02:44:23 +0000

    Seed assign projects to categories

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(express.static(__dirname + "/node_modules"))
 // seedImages()
 // seedProjects()
 // seedProjectImages()
-seedCategories()
-// seedCategoryProjects()
+// seedCategories()
+seedCategoryProjects()
 
 
 
